f50aefefada219bca163bfe9d50e881efb295561
Ariel Gallardo <arielacademlo@gmail.com>
2023-06-26 09:59:25 -0500

    trabajo finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,21 @@ import phrases from "./data/phrases.json"
 
 function App() {
 
+  const imagenes = ["/public/imagenes/fondo1.jpg", "/public/imagenes/fondo2.jpg", "/public/imagenes/fondo3.jpg", "/public/imagenes/fondo4.jpg"]
   const [index, useIndex] = useState(0)
+  const [img, useImg] = useState(0)
 
   const changePhrases = () => {
+    if (img < imagenes.length - 1) {
+      useImg(img + 1)
+    } else {
+      useImg(0)
+    }
+
     useIndex(Math.floor(Math.random() * phrases.length))
   }
+
+  document.body.style = `background-image: url(${imagenes[img]})`
 
   return (
     <>
